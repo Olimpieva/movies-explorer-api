4b0b7078f7a2a83743573eb7370a051e338109bc
Natalya Olimpieva <ukurennyi.koshak@gmail.com>
2021-12-21 19:51:35 +0300

    feat: user/movie models

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { urlServer } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
+mongoose.connect(urlServer);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const urlServer = 'mongodb://localhost:27017/moviesdb';
+
+module.exports = {
+  urlServer,
+};
