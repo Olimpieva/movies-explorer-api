0d388ee9ff540b8058f1ee1c964be3860da14a55
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2022-02-02 14:51:07 +0300

    fix: cors

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const routes = require('./routes');
 const errorHandler = require('./middlewares/error-determinant');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { apiLimiter } = require('./middlewares/limiter');
+const cors = require('./middlewares/cors');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(requestLogger);
+app.use(cors);
 app.use(helmet());
 app.use(apiLimiter);
 
